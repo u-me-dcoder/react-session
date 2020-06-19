76f62d66d6554f1422e9f4f2f188acffa493a51c
arthusArchaive <sajan.nepali@infodevelopers.com.np>
2020-06-19 10:10:43 +0545

    created a edit page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from 'react';
-import AddCategory from './components/Category/AddCategory'
-import './App.css';
+import React, { useState,Fragment } from "react";
+import AddCategory from "./components/Category/AddCategory";
+import EditCategory from "./components/Category/EditCategory";
+import "./App.css";
 
 function App() {
-  return (
-    <AddCategory/>
-  );
+  let [edit, setEdit] = useState(true);
+  if (edit) {
+    return (
+      <Fragment>
+             <h1>Helo world</h1>
+        <AddCategory />
+        <button onClick={e=>setEdit(!edit)}>toggle</button>
+      </Fragment>
+    );
+  } else {
+    return (
+      <Fragment>
+        <h1>Helo world 1</h1>
+        <EditCategory /> 
+        <button onClick={e=>setEdit(!edit)}>toggle</button>
+      </Fragment>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Category/EditCategory.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Formik } from "formik";
+export default function EditCategory() {
+ 
+
+ 
+
+  return (
+    <div>
+      <h1>Edit Category</h1>
+
+    </div>
+  );
+}
